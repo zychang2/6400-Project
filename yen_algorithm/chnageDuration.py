b67a2e3fd9e74c5694894be8a6dd552aac95083e
Zeyu Chang <frankchanggod@gmail.com>
2024-11-30 15:34:07 -0500

    update provinces

--- yen_algorithm/chnageDuration.py
+++ yen_algorithm/chnageDuration.py
@@ -1,16 +1,35 @@
 import csv
 from datetime import datetime
 
+# with open("airport_routes.csv","r",encoding="utf-8") as ogCsv, open("new.csv","w",newline="",encoding="utf-8") as newCsv:
+#     csvWriter = csv.writer(newCsv)
+#     csvReader = csv.reader(ogCsv)
+#     header = next(csvReader)
+#     csvWriter.writerow(header)
+#     for row in csvReader:
+#         time_obj = datetime.strptime(row[9], "%H:%M:%S")
+#         hours = time_obj.hour
+#         minutes = time_obj.minute
+#         seconds = time_obj.second
+#         total_minutes = hours * 60 + minutes + seconds // 60
+#         row[9] = total_minutes
+#         csvWriter.writerow(row)
+
 with open("airport_routes.csv","r",encoding="utf-8") as ogCsv, open("new.csv","w",newline="",encoding="utf-8") as newCsv:
     csvWriter = csv.writer(newCsv)
     csvReader = csv.reader(ogCsv)
     header = next(csvReader)
     csvWriter.writerow(header)
     for row in csvReader:
-        time_obj = datetime.strptime(row[9], "%H:%M:%S")
-        hours = time_obj.hour
-        minutes = time_obj.minute
-        seconds = time_obj.second
-        total_minutes = hours * 60 + minutes + seconds // 60
-        row[9] = total_minutes
+        for i in [2, 5]:
+            if row[i] == 'shaanxi':
+                row[i] = 'shanxi'
+                row[i - 1] = 1018
+            elif row[i] == 'fuijan':
+                row[i] = 'fujian'
+                row[i - 1] = 1003
+            elif row[i] == 'guangxi':
+                row[i] = 'guangxizhuangzuzizhiqu'
+                row[i - 1] = 1005
+            
         csvWriter.writerow(row)
